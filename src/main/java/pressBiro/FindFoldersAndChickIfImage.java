f27032ef3d28c9f8c67ec1fba20cafcda47081f6
sezarmbar <sezar_mbar@yahoo.com>
2018-05-17 12:14:19 +0200

    clean project

---
+++ src/main/java/pressBiro/FindFoldersAndChickIfImage.java
@@ -0,0 +1,69 @@
+package pressBiro;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FindFoldersAndChickIfImage {
+  public static void main(String[] args) {
+
+//    createDirectory("e:/mahmoud");
+
+    String path = "E:";
+
+    namePrint(path, "", "");
+
+  }
+
+  /**
+   * search in folders
+   * */
+  private static void namePrint(String path, String space, String dir) throws NullPointerException {
+    File folder = new File(path);
+    String spa = space + dir + "  ";
+    File[] listOfFiles = folder.listFiles();
+    try {
+
+      if (listOfFiles != null) {
+        for (File file : listOfFiles) {
+          if(file.isDirectory()){
+            System.out.println(spa + file.getName() + "  ---->  " + path);
+            namePrint(path + "/" + file.getName() , spa, " |");
+          }else if(file.isFile()){
+            System.out.print(spa + file.getName());
+            System.out.println("  " + path + "/" + file.getName());
+          }
+        }
+      }
+    } catch (NullPointerException e) {
+      System.out.println("path not found ... redirect to e:");
+//      namePrint("e:", "", "");
+    }
+  }
+
+
+
+  /**
+   * create Folder
+   *
+   * @param pathName: String path new Folder
+   */
+  private static void createDirectory(String pathName) {
+    Path path = Paths.get(pathName);
+    if (!Files.exists(path)) {
+      try {
+        Files.createDirectory(path);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+
+//  public static boolean isImage(File file) {
+//    System.out.println(file.getName());
+//    return true;
+//  }
+}
