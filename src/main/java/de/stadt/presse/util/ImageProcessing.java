1ff7ebd41fd99d993f0d48add94cee01003e05a0
sezarmbar <sezar_mbar@yahoo.com>
2018-06-26 12:43:15 +0200

    add    copyPNG() and  determineImageFormat()

--- src/main/java/de/stadt/presse/util/ImageProcessing.java
+++ src/main/java/de/stadt/presse/util/ImageProcessing.java
@@ -4,52 +4,47 @@ import com.drew.imaging.ImageMetadataReader;
 import com.drew.imaging.ImageProcessingException;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.iptc.IptcDirectory;
+
 import javax.activation.MimetypesFileTypeMap;
-import javax.imageio.IIOImage;
-import javax.imageio.ImageIO;
-import javax.imageio.ImageWriteParam;
-import javax.imageio.ImageWriter;
+import javax.imageio.*;
+import javax.imageio.stream.ImageInputStream;
 import javax.imageio.stream.ImageOutputStream;
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.util.Iterator;
 import java.util.List;
-import java.util.logging.Logger;
 
 public class ImageProcessing {
-  public static void main(String[] args) throws IOException {
-    String drive = "E:/Fotolia/Hintergründe/",
-      n = "",
-      orgName = "Abstraktes Muster_aSuruwataRi_Fotolia",
-      type = ".jpg";
-
-    String prefix = drive + n + orgName;
-
-    String org = prefix + type,
-      compressed = prefix + "-c" + type,
-      watermark = prefix + "-wmT" + type,
-      watermarkImage = prefix + "-wmI" + type;
-
-    String waterImage = "E:/foloParent/tmp/logo.png";
-
-    File filename = new File(org);
-    File desFileCompress = new File(compressed);
-    File desFileWatermark = new File(watermark);
-    File desFileWatermarkImage = new File(watermarkImage);
-    File inWaterImageFile = new File(waterImage);
-
-//    readImageMetadata(org);
-//    compressImageThump(filename, desFileCompress);
-//    addTextWatermark("Press Oldenburg", desFileCompress, desFileWatermark);
-//    addImageWatermark(inWaterImageFile, desFileCompress, desFileWatermarkImage);
+//  public static void main(String[] args) throws IOException {
+//    String drive = "E:/Fotolia/Hintergründe/",
+//      n = "",
+//      orgName = "Abstraktes Muster_aSuruwataRi_Fotolia",
+//      type = ".jpg";
 //
-    resize("D:\\pics\\Fotolia\\Icons\\Einzelne\\Abl.jpg", "d:/01-i.jpg", 1000);
-  }
+//    String prefix = drive + n + orgName;
+//
+//    String org = prefix + type,
+//      compressed = prefix + "-c" + type,
+//      watermark = prefix + "-wmT" + type,
+//      watermarkImage = prefix + "-wmI" + type;
+//
+//    String waterImage = "E:/foloParent/tmp/logo.png";
+//
+//    File filename = new File(org);
+//    File desFileCompress = new File(compressed);
+//    File desFileWatermark = new File(watermark);
+//    File desFileWatermarkImage = new File(watermarkImage);
+//    File inWaterImageFile = new File(waterImage);
+//
+////    readImageMetadata(org);
+////    compressImageThump(filename, desFileCompress);
+////    addTextWatermark("Press Oldenburg", desFileCompress, desFileWatermark);
+////    addImageWatermark(inWaterImageFile, desFileCompress, desFileWatermarkImage);
+////
+//    resize("D:\\pics\\Fotolia\\Icons\\Einzelne\\Abl.jpg", "d:/01-i.jpg", 1000);
+//  }
 
 
   /**
@@ -61,9 +56,9 @@ public class ImageProcessing {
     try {
       File image = new File(org);
       Metadata metadata = ImageMetadataReader.readMetadata(image);
-        List<String> keyword = metadata.getFirstDirectoryOfType(IptcDirectory.class).getKeywords();
-        return String.join(";", keyword);
-    } catch (ImageProcessingException | IOException  | NullPointerException e) {
+      List<String> keyword = metadata.getFirstDirectoryOfType(IptcDirectory.class).getKeywords();
+      return String.join(";", keyword);
+    } catch (ImageProcessingException | IOException | NullPointerException e) {
       return "null";
 //      System.out.println("can't find the metadata ...");
     }
@@ -139,7 +134,7 @@ public class ImageProcessing {
 
     } catch (IOException | NullPointerException ex) {
       ex.printStackTrace();
-      System.out.println("sourceImagePath   : " + sourceImagePath +"destImagePath  : " + destImagePath  );
+      System.out.println("sourceImagePath   : " + sourceImagePath + "destImagePath  : " + destImagePath);
       return "null";
     }
   }
@@ -182,6 +177,57 @@ public class ImageProcessing {
     }
   }
 
+  public static void copyPNG(File src, File dst) {
+
+    try {
+      System.out.println(src);
+      System.out.println(dst);
+      InputStream in = new FileInputStream(src);
+      OutputStream out = new FileOutputStream(dst+"/"+src.getName());
+
+      // Transfer bytes from in to out
+      byte[] buf = new byte[1024];
+      int len;
+      while ((len = in.read(buf)) > 0) {
+        out.write(buf, 0, len);
+      }
+      in.close();
+      out.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }finally {
+      System.out.println("image PNG have coped .....");
+    }
+
+  }
+
+
+  public static String determineImageFormat(String imagePath) {
+    try {
+      // get image format in a file
+      File file = new File(imagePath);
+
+      // create an image input stream from the specified file
+      ImageInputStream iis = ImageIO.createImageInputStream(file);
+
+      // get all currently registered readers that recognize the image format
+      Iterator<ImageReader> iter = ImageIO.getImageReaders(iis);
+
+      if (!iter.hasNext()) {
+        throw new RuntimeException("No readers found!");
+      }
+      // get the first reader
+      ImageReader reader = iter.next();
+      String format= reader.getFormatName();
+      // close stream
+      iis.close();
+      return format;
+
+    } catch (IOException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
 
   /**
    * @param filePath file path String.
@@ -191,6 +237,7 @@ public class ImageProcessing {
     File f = new File(filePath);
     MimetypesFileTypeMap mediatype = new MimetypesFileTypeMap();
     mediatype.addMimeTypes("image/jpeg jpeg");
+    mediatype.addMimeTypes("image/png png");
     String mimetype = mediatype.getContentType(f);
     String type = mimetype.split("/")[0];
     return type.equals("image");
@@ -207,10 +254,10 @@ public class ImageProcessing {
       BufferedImage outputImage;
       int scaledWidth;
 
-      if(inputImage.getHeight()> scaledHeight*2) {
+      if (inputImage.getHeight() > scaledHeight * 2) {
         scaledWidth = scaledHeight * inputImage.getWidth() / inputImage.getHeight();
         // creates output image
-          outputImage = new BufferedImage(scaledWidth,scaledHeight, inputImage.getType());
+        outputImage = new BufferedImage(scaledWidth, scaledHeight, inputImage.getType());
         // scales the input image to the output image
         Graphics2D g2d = outputImage.createGraphics();
         g2d.drawImage(inputImage, 0, 0, scaledWidth, scaledHeight, null);
@@ -225,7 +272,7 @@ public class ImageProcessing {
       ImageIO.write(outputImage, formatName, new File(outputImagePath));
       return true;
     } catch (NullPointerException | IOException e) {
-      System.out.println("error IO................... "  + inputImagePath);
+      System.out.println("error IO................... " + inputImagePath);
       e.printStackTrace();
     }
     return true;
