11501a6a511d217d0caa85d57fba42cb21628966
sezarmbar <sezar_mbar@yahoo.com>
2018-06-29 12:56:50 +0200

    add @EqualsAndHashCode @ToString

--- src/main/java/de/stadt/presse/entity/Keyword.java
+++ src/main/java/de/stadt/presse/entity/Keyword.java
@@ -20,22 +20,23 @@ import java.util.Set;
 @EqualsAndHashCode(exclude = {"images"})
 public class Keyword {
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY) @JsonProperty
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @JsonProperty
   private Long id;
 
   @NotBlank
   @JsonProperty
-  @Column(name = "keyword_en", nullable = false,unique = true)
+  @Column(name = "keyword_en", nullable = false, unique = true)
   private String keywordEn;
 
-//  @NotBlank
+  //  @NotBlank
   @JsonProperty
   @Column(name = "keyword_De")
   private String keywordDe;
 
 
-@ManyToMany(fetch = FetchType.LAZY, mappedBy = "keywords")
-    private Set<Image> images= new HashSet<>();
+  @ManyToMany(fetch = FetchType.LAZY, mappedBy = "keywords")
+  private Set<Image> images = new HashSet<>();
 
   public Keyword(String keywordEn) {
     this.keywordEn = keywordEn;
